404e0c742943f1dbc9cd63f24f9aba48792be979
anabarcab-wq <an.abarcab@duocuc.cl>
2026-04-14 17:01:46 -0400

    Se sube proyecto base.

---
+++ src/main/java/com/duoc/crossmatch/CrossmatchApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.crossmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossmatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/crossmatch/CrossmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.crossmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
