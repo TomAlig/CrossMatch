7284591ef350c1f49f6c3c94dd49f6ec57210b2d
Tom <tom.aliaga@duocuc.cl>
2026-04-15 12:47:25 -0400

    Modelos y Dependencia MySQL

---
+++ src/main/java/com/duoc/crossmatch/Model/ModelAnime.java
@@ -0,0 +1,29 @@
+package com.duoc.crossmatch.Model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ModelAnime {
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank
+    private String nombre;
+
+    private String descripcion;
+
+    @NotBlank
+    private String genero;
+
+    @NotBlank
+    private String clasificacionEdad;
+}
