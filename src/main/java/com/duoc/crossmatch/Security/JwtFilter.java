91462ec5277d3d799aa9a8694b1bcde3e0a4cee0
carlossepulvedadiaz <cssepulvedadiaz@gmail.com>
2026-07-13 18:04:54 -0400

    Se agrega filtro para login y registro

--- src/main/java/com/duoc/crossmatch/Security/JwtFilter.java
+++ src/main/java/com/duoc/crossmatch/Security/JwtFilter.java
@@ -34,7 +34,11 @@ public class JwtFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain)
             throws ServletException, IOException {
-
+        String path = request.getRequestURI();
+        if (path.contains("/api/v1/auth/")||path.contains("/swagger-ui")||path.contains("/v3/api-docs")) {
+            filterChain.doFilter(request, response);
+            return; 
+        }
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
